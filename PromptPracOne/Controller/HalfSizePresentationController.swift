71a064127fc497cc52bc5b2e85636673164a34c2
현진 <guswlsrlacl@naver.com>
2024-09-25 23:00:50 +0900

    Last Dance

--- PromptPracOne/Controller/HalfSizePresentationController.swift
+++ PromptPracOne/Controller/HalfSizePresentationController.swift
@@ -9,22 +9,22 @@ import Foundation
 import UIKit
 
 class HalfSizePresentationController: UIPresentationController {
-    
     override var frameOfPresentedViewInContainerView: CGRect {
         guard let containerView = containerView else { return .zero }
-        
+
         let heightMultiplier: CGFloat = 0.6 // 높이를 0.6배로 설정
         let width = containerView.bounds.width
-        let height = containerView.bounds.height * heightMultiplier
+        let height = max(containerView.bounds.height * heightMultiplier, 480) // 최소 높이를 450으로 설정
+        
         let x = CGFloat(0)
         let y = containerView.bounds.height - height
-        
+
         return CGRect(x: x, y: y, width: width, height: height)
     }
-    
+
     override func presentationTransitionWillBegin() {
         guard let presentedView = presentedView else { return }
-        
+
         presentedView.layer.cornerRadius = 16
         presentedView.clipsToBounds = true
     }
